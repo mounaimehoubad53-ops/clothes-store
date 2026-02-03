612a9612cdfc8cad82c3b44c2547d2b89315ea22
mounaimehoubad53-ops <mounaimehoubad53@gmail.com>
2026-02-03 16:19:16 +0100

    Refactor order function for message formatting

--- script.js
+++ script.js
@@ -2,14 +2,15 @@ function order() {
   const color = document.getElementById("color").value;
   const size = document.getElementById("size").value;
 
-  const message = `مرحبا، أريد طلب T-Shirt
+  const message = `طلب جديد:
+T-Shirt Minimal
 اللون: ${color}
 المقاس: ${size}`;
 
-  const phone = "213556372063"; // رقمك
-  const url = `https://wa.me/${phone}?text=${encodeURIComponent(message)}`;
-
-  window.open(url);
+  const phone = "213XXXXXXXXX";
+  window.open(
+    `https://wa.me/${phone}?text=${encodeURIComponent(message)}`
+  );
 }
 
 
